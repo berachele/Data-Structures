339710cdd56d14b621b94bdfd663be44e9ed309f
Rachele Edwards <dot.com1472@gmail.com>
2020-06-11 08:21:18 -0600

    finished part one on BST get max and foreach

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -56,13 +56,19 @@ class BSTNode:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        if not self.right:
+        if self.right:
+            return self.right.get_max()
+        else:
             return self.value
-        return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
 
     # Part / Day 2 -----------------------
 
